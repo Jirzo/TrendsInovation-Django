25489832a2355e710d2c91baaa4516c70df18689
Rivas <alfredo.rivas@accenture.com>
2026-05-28 17:18:54 -0600

    PROD

--- djangom/settings/prod.py
+++ djangom/settings/prod.py
@@ -10,20 +10,36 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 PRODUCTION = os.environ.get('PRODUCTION')
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+DB_HOST = os.environ.get('DB_HOST')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        "OPTIONS": {
-            "init_command": "SET default_storage_engine=INNODB",
+if DB_HOST: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': DB_HOST,
+            'PORT': os.environ.get('DB_PORT', '3306'),
+            "OPTIONS": {
+                "init_command": "SET default_storage_engine=INNODB",
+            }
         }
     }
-}
+else:
+        DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'django',
+            'USER': 'root',
+            'PASSWORD': 'admin',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            "OPTIONS": {
+                "init_command": "SET default_storage_engine=INNODB",
+            }
+        }
+    }
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
